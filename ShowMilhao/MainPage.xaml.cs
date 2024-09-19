fde1c251045c7309bf8f14ee39b2bb2acf2927e6
Usuario <Usuario@CPD03-M11>
2024-09-19 14:33:12 -0300

    audio

--- ShowMilhao/MainPage.xaml.cs
+++ ShowMilhao/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace ShowMilhao
+﻿
+using Plugin.Maui.Audio;
+
+namespace ShowMilhao
+
 {
     public partial class MainPage : ContentPage
     {
@@ -10,11 +14,94 @@
         public MainPage()
         {
             InitializeComponent(); 
+
             lbl_pergunta.Text = pergunta_count.ToString();
             lbl_nivel.Text = nivel.ToString();
-            lbl_premio.Text = premio.ToString("C");    
+            lbl_premio.Text = premio.ToString("C");
+
+            this.BindingContext = App.getRendomPerguntaFacil();
+
+            //Audio som
+            Stream track =
+                FileSystem.OpenAppPackageFileAsync("abertura-show-do-milhao.mp3").Result;
+            AudioManager.Current.CreatePlayer(track).Play();
         }
-        
+
+        private void TocaSom()
+        {
+            string track = "";
+
+            switch (pergunta_count)
+            {
+                case 1: 
+                    track = "1000.wav";
+                break;
+
+                case 2:
+                    track = "2000.wav";
+                break;
+
+                case 3:
+                    track = "3000.wav";
+                break;
+
+                case 4:
+                    track = "4000.wav";
+                break;
+
+                case 5:
+                    track = "5000.wav";
+                break;
+
+                case 6:
+                    track = "10000.wav";
+                break;
+
+                case 7:
+                    track = "20000.wav";
+                    break;
+
+                case 8:
+                    track = "30000.wav";
+                    break;
+
+                case 9:
+                    track = "40000.wav";
+                    break;
+
+                case 10:
+                    track = "50000.wav";
+                    break;
+
+                case 11:
+                    track = "100000a.wav";
+                    break;
+
+                case 12:
+                    track = "200000.wav";
+                    break;
+
+                case 13:
+                    track = "300000.wav";
+                    break;
+
+                case 14:
+                    track = "400000.wav";
+                    break;
+
+                case 15:
+                    track = "500000.wav";
+                    break;
+
+                case 16:
+                    track = "1000000.wav";
+                    break;
+
+               
+
+
+            }
+        }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
@@ -70,7 +157,7 @@
             if (acertou)
             {
                 await DisplayAlert("ACERTOU !!", resp, "ok");                
-                pergunta_count++;
+                
                 avanca_pergunta();
                 
             }
@@ -137,7 +224,7 @@
             lbl_nivel.Text = nivel.ToString();
             lbl_premio.Text =  "R$ " + premio.ToString("C");
 
-
+            pergunta_count++;
         }
 
        

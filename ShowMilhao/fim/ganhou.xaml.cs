6d29484581071081d630c3568b9429398cede6f3
Usuario <Usuario@CPD03-M07>
2024-10-22 15:44:28 -0300

    fim

--- ShowMilhao/fim/ganhou.xaml.cs
+++ ShowMilhao/fim/ganhou.xaml.cs
@@ -1,3 +1,5 @@
+using Plugin.Maui.Audio;
+
 namespace ShowMilhao.fim;
 
 public partial class Ganhou : ContentPage
@@ -5,7 +7,8 @@ public partial class Ganhou : ContentPage
 	public Ganhou()
 	{
 		InitializeComponent();
-	}
+        AudioManager.Current.CreatePlayer(FileSystem.OpenAppPackageFileAsync("terminou.wav").Result).Play();
+    }
 
     private void comeco_Clicked(object sender, EventArgs e)
     {
